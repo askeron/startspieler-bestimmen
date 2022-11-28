f10bdece63da00e1c034ef6536f575d0b62459fe
Bjoern Kuhlen <bjoern@kuhlen.org>
2022-11-28 01:59:10 +0100

    initial commit

---
+++ data.js
@@ -0,0 +1,7 @@
+dataCallback({
+    conditions: [
+        "wer zuletzt auf einem Konzert war",
+        "wer zuletzt aus dem Urlaub zurückkam",
+        "wer die höchste Hausnummer hat",
+    ]
+})
