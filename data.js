682a58cdf278eea3c99bdc604b22874ec351384a
Bjoern Kuhlen <bjoern@kuhlen.org>
2022-11-28 02:23:18 +0100

    added conditions

--- data.js
+++ data.js
@@ -3,5 +3,12 @@ dataCallback({
         "wer zuletzt auf einem Konzert war",
         "wer zuletzt aus dem Urlaub zurückkam",
         "wer die höchste Hausnummer hat",
+        "wer zuletzt seine Eltern besucht hat",
+        "wer die meisten Schlüssel am Schlüsselbund hat",
+        "wer die meisten Brettspiele hat",
+        "wer die meisten Kleidungsstücke trägt",
+        "wer die höchste Zahl in den letzten 4 Ziffern der Handynummer hat",
+        "wessen Arbeitsweg am längsten ist",
+        "wer zuletzt Fahrrad gefahren ist",
     ]
 })
